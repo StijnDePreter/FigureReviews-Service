9dda4113c6be6c81d705a5c6d9b81ec8226f7b5e
Mathias <r0618721@student.thomasmore.be>
2022-09-30 13:43:27 +0200

    first commit

---
+++ src/main/java/com/example/figurereviewsservice/FigureReviewsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.figurereviewsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FigureReviewsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FigureReviewsServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/figurereviewsservice/FigureReviewsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.figurereviewsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FigureReviewsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
